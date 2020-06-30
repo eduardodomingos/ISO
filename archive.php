aa67c4d0a9529b0a3a51e81aac30d9741d61af6e
Eduardo Domingos <epcdomingos@gmail.com>
2020-06-30 18:11:21 +0100

    Tweak archive template to suit theme markup

--- archive.php
+++ archive.php
@@ -10,42 +10,38 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main class="site-main">
 
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
+			<section class="section">
+				<div class="wrap">
+					<header class="section__header">
+						<?php the_archive_title( '<h1>', '</h1>' ); ?>
+						<?php the_archive_description();?>
+					</header>
+					<div class="section__content">
+						<ul class="list list--default">
+							<?php
+							/* Start the Loop */
+							while ( have_posts() ) :
+								the_post();
+								echo '<li>';
+								iso_get_template_part('template-parts/content', 'teaser');
+								echo '</li>';
+							endwhile;
+							// the_posts_navigation();
+							?>
+						</ul>
+					</div>		
+				</div>
+			</section>
+		<?php else :
 
 			get_template_part( 'template-parts/content', 'none' );
 
-		endif;
-		?>
+		endif; ?>
 
-	</main><!-- #main -->
+	</main>
 
 <?php
-get_sidebar();
 get_footer();
