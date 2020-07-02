34b5c7ab6df59d72ef6eca29407b5a6f53aff6a7
Eduardo Domingos <epcdomingos@gmail.com>
2020-07-02 01:46:08 +0100

    Tweak page tempates to meet theme structure

--- page.php
+++ page.php
@@ -15,24 +15,58 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main class="site-main">
+		<section class="section">
+			<div class="wrap">
+				<header class="section__header">
+					<?php the_title( '<h1>', '</h1>' ); ?>
+				</header>
+				<div class="section__content">
+					<?php
+					while ( have_posts() ) :
+						the_post();
+
+						get_template_part( 'template-parts/content', 'page' );
+
+					endwhile; // End of the loop.
+					?>
+				</div>
+			</div>
+		</section>
 
 		<?php
-		while ( have_posts() ) :
-			the_post();
+		$latest = new WP_Query(
+			array(
+				'post_type' => 'portfolio',
+				'posts_per_page' => 3
+			)
+		);
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+		if ( $latest->have_posts() ): ?>
+			<section class="section section--bg">
+				<div class="wrap">
+					<header class="section__header">
+						<h2>Recent Work</h2>
+						<p><a href="<?php echo get_post_type_archive_link('portfolio'); ?>">view all work</a></p>
+					</header>
+					<div class="section__content">
+						<ul class="list list--portfolio">
+							<?php
+							/* Start the Loop */
+							while ( $latest->have_posts() ) :
+								$latest->the_post();
+								echo '<li>';	
+								iso_get_template_part('template-parts/content', 'teaser', array('template_type' => 'portfolio'));
+								echo '</li>';
+							endwhile;
+							?>
+						</ul>
+					</div>
+				</div>
+			</section>
+			<?php wp_reset_postdata(); ?>
+		<?php endif;?>
+	</main>
 
 <?php
-get_sidebar();
 get_footer();
