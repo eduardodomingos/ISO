35ec04e28fe4338bc3596835a9474f6bc98e28c9
Eduardo Domingos <epcdomingos@gmail.com>
2020-07-02 01:08:34 +0100

    Tweak 404 template to suit site markup

--- 404.php
+++ 404.php
@@ -10,50 +10,20 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'iso' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'iso' ); ?></p>
-
+	<main class="site-main">
+		<section class="section error-404">
+			<div class="wrap">
+				<header class="section__header">
+					<h2>Oops! That page can't be found :(</h2>
+					<p>It looks that the page you are looking for doesn't exist.<br><b><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Return home</a> or perhaps searching can help.</b></p>
+				</header>
+				<div class="section__content">
 					<?php
 					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
 					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'iso' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$iso_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'iso' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$iso_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
+				</div>
+			</div>
+		</section>
 	</main><!-- #main -->
 
 <?php
